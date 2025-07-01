af087eee7390a75fa03d23ec5f3e6f978564174d
AlanikREDAWN <lilia.dostie@socprep.org>
2025-07-01 12:40:11 -0400

    import yaml file and test to see whether it works

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import yaml
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 from dotenv import load_dotenv
@@ -7,15 +8,20 @@ load_dotenv()
 SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
 SLACK_APP_TOKEN = os.getenv("SLACK_APP_TOKEN")
 
-# Initializes your app with your bot token and socket mode handler
+
 app = App(token=SLACK_BOT_TOKEN)
 
-# The echo command simply echoes on command
+with open('tutorial-story.yaml', 'r') as file:
+    tutorialstory = yaml.safe_load(file)
+
+
 @app.command("/startadventure")
 def repeat_text(ack, respond, command):
-    # Acknowledge command request
+
     ack()
+    respond(f"Rooms: {tutorialstory['rooms']}")
     respond(f"it works")
+    
 
 # Start your app
 if __name__ == "__main__":
