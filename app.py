dd5a0bc173cd8102d9f1fe2862571f4095ce5a4b
AlanikREDAWN <lilia.dostie@socprep.org>
2025-07-01 15:26:06 -0400

    fix blocks and add go north

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ with open('./stories/tutorial-story.yaml', 'r') as file:
 tutorial_player_location = tutorialstory['rooms']['great_hall']
 
 @app.command("/startadventure")
-def repeat_text(ack, respond, command, client):
+def start_adventure(ack, respond, command, client):
 
     ack()
     user_id = command["user_id"]
@@ -52,8 +52,8 @@ def repeat_text(ack, respond, command, client):
 	]
 
     try:
-        client.chat_postMessage(channel=user_id, text=message)
-        client.chat_postMessage(blocks)
+        # client.chat_postMessage(channel=user_id, text=message)
+        client.chat_postMessage(channel=user_id, text=blocks)
         
     except Exception as e:
         print(f"Error sending DM: {e}")
@@ -61,7 +61,50 @@ def repeat_text(ack, respond, command, client):
 
     # respond(f"Rooms: {tutorialstory['rooms']}")
     # respond(f"it works")
-    
+
+@app.command("/go north")
+def go_north(ack, respond, command, client):
+    ack()
+    user_id = command["user_id"]
+
+    if tutorial_player_location == tutorialstory['rooms']['great_hall']:
+        tutorial_player_location = tutorialstory['rooms']['hallway']
+        blocks = [
+            {
+                "type": "header",
+                "text": {
+                    "type": "plain_text",
+                    "text": tutorial_player_location['name'],
+                    "emoji": true
+                }
+            },
+            {
+                "type": "rich_text",
+                "elements": [
+                    {
+                        "type": "rich_text_section",
+                        "elements": [
+                            {
+                                "type": "text",
+                                "text": tutorial_player_location['description'],
+                                "style": {
+                                    "italic": true
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+        try:
+            client.chat_postMessage(channel=user_id, text=blocks)
+            client.chat_postMessage(blocks)
+        
+        except Exception as e:
+            print(f"Error sending DM: {e}")
+            respond("Sorry, I couldn't send you a direct message.")
+    else:
+        pass
 
 # Start your app
 if __name__ == "__main__":
