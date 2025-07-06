b8b2cde4511a69f5fac8ae9aff537991dd8afa03
AlanikREDAWN <lilia.dostie@socprep.org>
2025-07-06 14:31:01 -0400

    fix

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def start_adventure(ack, respond, command, client):
 def go(ack, respond, command, client):
     ack()
     # user_text = command.get("text", "").strip()
-    user_text = command["user_text"]
+    user_text = command["text"]
     user_id = command["user_id"]
 
     if not user_text:
