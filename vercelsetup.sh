9dd417ee39fca87c8531ef8f75b8ceec3fded2f9
AlanikREDAWN <lilia.dostie@socprep.org>
2025-07-21 12:15:19 -0400

    added vercelsetup.sh

---
+++ vercelsetup.sh
@@ -0,0 +1,10 @@
+# PORT="$1"
+# export PORT="$PORT"
+
+# kill -9 $(lsof -ti ":$PORT") 2>/dev/null
+git pull
+# python3 -m venv .venv
+# source .venv/bin/activate
+pip install -r requirements.txt
+# gunicorn -b ":$PORT" app:app
+python3 app.py
