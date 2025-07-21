8b7bfd7a55391c2a7ed7ec1a74565b66223c4113
AlanikREDAWN <lilia.dostie@socprep.org>
2025-07-21 12:43:45 -0400

    fix

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import yaml
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 from dotenv import load_dotenv
-load_dotenv()
+# load_dotenv()
 
 SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
 SLACK_APP_TOKEN = os.getenv("SLACK_APP_TOKEN")
