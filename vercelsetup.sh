98dfc1b482d42c210e4ca5381c31ee90a0cc21e6
AlanikREDAWN <lilia.dostie@socprep.org>
2025-07-21 12:20:07 -0400

    fix

--- vercelsetup.sh
+++ vercelsetup.sh
@@ -3,8 +3,8 @@
 
 # kill -9 $(lsof -ti ":$PORT") 2>/dev/null
 # git pull
-# python3 -m venv .venv
-# source .venv/bin/activate
+python3 -m venv .venv
+source .venv/bin/activate
 pip install -r requirements.txt
 # gunicorn -b ":$PORT" app:app
 python3 app.py
