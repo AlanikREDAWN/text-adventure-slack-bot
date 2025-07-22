d69682b62ea22310e3106b9ba500b69b5b106be9
AlanikREDAWN <lilia.dostie@socprep.org>
2025-07-22 12:28:24 -0400

    fix spelling error

--- app.py
+++ app.py
@@ -508,7 +508,7 @@ def talkto(ack, respond, command, client, say, body, logger):
         if "glykoy" in user_text:
             if current_room == tutorialstory['rooms']['great_hall']:
                 client.chat_postMessage(channel=user_id, text=f"*Glykoy:* {tutorialstory['npcs']['glykoy']['dialogue']['wake'][0]}")
-                client.chat_postMessage(channel=user_id, text=f"*Glykoy:* {tutorialstory['npcs']['glykoy']['dialouge']['wake'][1]}")
+                client.chat_postMessage(channel=user_id, text=f"*Glykoy:* {tutorialstory['npcs']['glykoy']['dialogue']['wake'][1]}")
                 client.chat_postMessage(channel=user_id, text=f"*Interact Options:* {tutorialstory['npcs']['glykoy']['interact_options'][0]}")
     else:
         respond("Error")
