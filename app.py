a332a70ab67a73c52a6a076278af8f26d3619f39
AlanikREDAWN <lilia.dostie@socprep.org>
2025-07-23 09:25:07 -0400

    end testing

--- app.py
+++ app.py
@@ -548,7 +548,7 @@ def handle_message(message, client, logger, respond, say):
             client.chat_postMessage(channel=user_id, text=f"*Glykoy:* {tutorialstory['npcs']['glykoy']['interact_options'][0]['response']}")
             del waiting_for_response_glykoy[user_id]
         else:
-            say("nope")
+            pass
     else:
         pass
 
