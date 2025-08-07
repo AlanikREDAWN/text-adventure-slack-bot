14e1989f63d0705d88c1469d0b7a49c6ff640776
AlanikREDAWN <lilia.dostie@socprep.org>
2025-08-07 11:40:30 -0400

    fix?

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import logging
 import re
 import time
 logging.basicConfig(level=logging.INFO)
-# load_dotenv()
+load_dotenv()
 
 SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
 SLACK_APP_TOKEN = os.getenv("SLACK_APP_TOKEN")
